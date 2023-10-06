f4876098300af9b52012cd9a3f279a49a5bc2b56
voronov <114691383+voronov-nikita@users.noreply.github.com>
2023-10-06 22:39:59 +0300

    test-JupiteNotebook

--- scr/simple-data_base-SQL.py
+++ scr/simple-data_base-SQL.py
@@ -1,58 +1,7 @@
-import sqlite3 as sql
-
-def create_database(name_db:str) -> None:
-    # подключиться к базы данных
-    # если ее нет, то создать новую
-    db = sql.connect(name_db)
-    # создаем курсор
-    cursor = db.cursor()
-
-    # заменить имя {name_table} на что-то другое
-    # заменить данные в этой талице
-    cursor.execute("""CREATE TABLE IF NOT EXISTS name_table(
-    id INTEGER,
-    name TEXT,
-    age INTEGER,
-    login TEXT,
-    password TEXT
-    )
-    """)
-
-    # сохранить
-    db.commit()
-    
-
-# получение всех данных из базы данных {name_db}
-# для опознания некоторой строки используеться {data}, как признак отличия (id пользователя к примеру)
-# вернет кортеж с данными из базы данных
-def get_data_from_database(name_db:str, data) -> tuple:
-    
-    db = sql.connect(name_db)
-    cursor = db.cursor()
-    
-    cursor.execute(
-        f"""SELECT * FROM {name_db} WHERE id={data}"""
-    )
-    
-    # вернуть одно вхождение
-    return cursor.fetchone()
-
-
 # добавить данные в базу данных {name_db}
 # для опознания некоторой строки используеться {data}, как признак отличия (id пользователя к примеру)
 # данные, которые нужно добавить -> {add_data}
-# Важно помнить о типе данных, которые устанавливали при создании БД
-def add_data_in_database(name_db:str, data, add_data) -> None:
-    
-    db = sql.connect(name_db)
-    cursor = db.cursor()
-    
-    cursor.execute(
-        f"""INSERT INTO {name_db} VALUES (id={data}, name='{add_data}')"""
-    )
-    
-    # сохраняем
-    db.commit()
+
     
     
 # Обновление данных в БД {name_bd}
