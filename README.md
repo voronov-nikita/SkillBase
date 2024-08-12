b6e595ca2e076f6b6e8ea2183cb4c16e0a1ecffc
voronov <114691383+voronov-nikita@users.noreply.github.com>
2024-08-12 16:17:44 +0300

    add new file to folder `ReactNative` && re-write README to better composition

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# Пока пусто из-за большой перепланировки
+# Полезные части кода для личных (PET) проектов
+
+Память меня никогда не подводила!
+По крайней мере, я такого не помню.
+
+## **Python**
+
+### База в теории
+
+1. [База данных SQLite3](./Python/database.py)
+2. [Теория по библиотеке sqlite3 python](./Python/sqlite3.ipynb)
+3. [Flask](./Python/Flask.md)
+4. [MatplotLib - построение графиков на python](./Python/matplotlib.md)
+5. [Numpy - библиотека массивов и векторов в python](./Python/numpy.md)
+6. [База по PyQt5](./Python/pyqt5.ipynb)
+7. [Варант формы авторизации на PyQt5](./Python/PyQt5-auth.py)
+8. [Построение таблиц с помощью PyQt5](./Python/PyQt5-table.py)
+9. [Построение графиков с помощью PyQt5](./Python/PyQt5-graphics.py)
+10. [Написание ТГ бота на python](./Python/telegram-bot-telebot.ipynb)
+
+### Интересные задачи и решения к ним
+
+1. [Решение задачи интерпитарор с олимпиады и сайта CodeWars](./Python/interpritator.py)
+
+## **React Native**
+
+### Примеры простых компонентов для приложений
+
+1. [NumPad клавиутра для ввода PIN кода](./ReactNative/lib/keyBoard.js)
+
+### Примеры полноценных монолитных приложений
+
+1. [ToDo приложение для контроля задач](./ReactNative/lib/ToDo.js)
+
+<br><br>
+
+###### 12.08.2024 - last change in repository.
---
+++ ReactNative/lib/keyBoard.js
@@ -0,0 +1,130 @@
+//
+// Компонент клавиатуры для входа в систему.
+//
+
+import React, { useState, useEffect } from "react";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+
+import CONFIG from "../config";
+
+export const KeyBoardPin = ({ navigation }) => {
+	const [pin, setPin] = useState("");
+	const [textColor, setTextColor] = useState("#000");
+	const [infoText, setInfo] = useState("Введите PIN-код для входа");
+
+	useEffect(() => {
+		if (pin.length === 4 && pin != "1234") {
+			setInfo("Неверный PIN");
+			setTextColor("#ff0000");
+			handleClear();
+		} else if (pin.length === 4 && pin == "1234") {
+			setInfo("Добро пожаловать");
+			setTextColor("#51b155");
+			navigation.replace("Home");
+		}
+	}, [pin]);
+
+	const handlePress = (num) => {
+		if (pin.length < 4) {
+			setPin((prev) => prev + num);
+		}
+	};
+
+	const handleBackspace = () => {
+		setPin((prev) => prev.slice(0, -1));
+	};
+
+	const handleClear = () => {
+		setPin("");
+	};
+
+	const renderCircles = () => {
+		return (
+			<View style={styles.circlesContainer}>
+				{Array(4)
+					.fill("")
+					.map((_, index) => (
+						<View
+							key={index}
+							style={[styles.circle, pin.length > index && styles.filledCircle]}
+						/>
+					))}
+			</View>
+		);
+	};
+
+	return (
+		<View style={styles.container}>
+			<Text style={[styles.titleText, { color: textColor }]}>{infoText}</Text>
+
+			{renderCircles()}
+			<View style={styles.keyboard}>
+				{["1", "2", "3", "4", "5", "6", "7", "8", "9"].map((num) => (
+					<TouchableOpacity
+						key={num}
+						style={styles.key}
+						onPress={() => handlePress(num)}
+					>
+						<Text style={styles.keyText}>{num}</Text>
+					</TouchableOpacity>
+				))}
+				<TouchableOpacity style={styles.key} onPress={handleClear}>
+					<Text style={styles.keyText}>✖</Text>
+				</TouchableOpacity>
+				<TouchableOpacity style={styles.key} onPress={() => handlePress("0")}>
+					<Text style={styles.keyText}>0</Text>
+				</TouchableOpacity>
+				<TouchableOpacity style={styles.key} onPress={handleBackspace}>
+					<Text style={styles.keyText}>⌫</Text>
+				</TouchableOpacity>
+			</View>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	container: {
+		height: "80%",
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	instruction: {
+		fontSize: 18,
+		marginBottom: 20,
+	},
+	circlesContainer: {
+		flexDirection: "row",
+		marginBottom: 30,
+	},
+	circle: {
+		width: 20,
+		height: 20,
+		borderRadius: 10,
+		borderWidth: 2,
+		borderColor: "#333",
+		margin: 10,
+		backgroundColor: "transparent",
+	},
+	filledCircle: {
+		backgroundColor: "#333",
+	},
+	keyboard: {
+		flexDirection: "row",
+		flexWrap: "wrap",
+		width: 240,
+		justifyContent: "center",
+	},
+	key: {
+		width: 70,
+		height: 70,
+		justifyContent: "center",
+		alignItems: "center",
+		margin: 5,
+		backgroundColor: "#ddd",
+		borderRadius: 5,
+	},
+	keyText: {
+		fontSize: 28,
+		fontWeight: "bold",
+	},
+});
